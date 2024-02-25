3b7a2416143c37397e32938ae96c074297b922c8
Matas Timinskas <matas.timinskas@gmail.com>
2024-02-25 19:20:58 +0200

    added Ball(Turtle) class

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from turtle import *
+import turtle
+from turtle import Turtle
+from ball import Ball
 
-print("hello")
+print("hello world")
 
+
+ball = Ball()
+ball.fd(200)
+
+ball.screen.delay(0)
+
+
+pad = Turtle()
+
+pad.screen.mainloop()
